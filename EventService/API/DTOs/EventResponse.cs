3f0cce31eed5f0e7d26e58fcd0a9db39e7ea53de
Sheersendu <shibrata15@gmail.com>
2025-05-25 13:14:58 +0530

    Event response object

--- EventService/API/DTOs/EventResponse.cs
+++ EventService/API/DTOs/EventResponse.cs
@@ -2,5 +2,8 @@
 
 public class EventResponse
 {
-	
+	public Guid EventId { get; set; } = Guid.NewGuid();
+	public string Name { get; set; }
+	public string Timing { get; set; }
+	public string Address { get; set; }
 }
